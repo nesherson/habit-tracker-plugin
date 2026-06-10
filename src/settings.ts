7fb3987d3223eedfee8d39aeb90643530c13cfb7
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-06-10 11:16:40 +0200

    Remove most of the sample code, adjust settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,18 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import MyPlugin from './main';
+import HabitTracker from './main';
 
-export interface MyPluginSettings {
+export interface Settings {
 	mySetting: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
+export const DEFAULT_SETTINGS: Settings = {
 	mySetting: 'default',
 };
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class HabitTrackerSettingTab extends PluginSettingTab {
+	plugin: HabitTracker;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HabitTracker) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
