7f3d39ced715b4ee2a644caa80dc1aaab4c51625
Nešad Omeragić <nesad.omeragich27@gmail.com>
2026-09-16 14:44:15 +0200

    Console log build failure

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -51,6 +51,14 @@ const context = await esbuild.context({
 if (prod) {
 	await context.rebuild();
 	process.exit(0);
+	try {
+		const result = await context.rebuild();
+	} catch (err) {
+		console.error("Build failed:", err);
+	} finally {
+		await context.dispose();
+		process.exit(0);
+	}
 } else {
 	await context.watch();
 }
